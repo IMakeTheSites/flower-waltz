6c7420ff83e06a5ef1efb080ef5443b1c2796803
Mark Wlodawski <imakethesites@gmail.com>
2023-03-09 09:16:22 -0500

    Insert Responsive Grid styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,7 +21,7 @@ function HomePage({ products }) {
       </Head>
       <main className="px-6 py-4">
         <Title>Waltz of the Flowers Shop</Title>
-        <ul>
+        <ul className="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-4">
           {products.map((product) => (
             <li key={product.id}>
               <ProductCard product={product} />
