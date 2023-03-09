f3d4912fe5637f52291a4cc8374f2b259e47275d
Mark Wlodawski <imakethesites@gmail.com>
2023-03-09 15:11:59 -0500

    Add form state and validation

--- src/pages/sign-in.js
+++ src/pages/sign-in.js
@@ -2,16 +2,35 @@ import Button from '@/components/Button';
 import Field from '@/components/Field';
 import Input from '@/components/Input';
 import Page from '@/components/Page';
+import { useState } from 'react';
 
 function SignInPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('should submit:', { email, password });
+  };
+
   return (
     <Page title="Sign In">
-      <form>
+      <form onSubmit={handleSubmit}>
         <Field label="Email">
-          <Input type="email" />
+          <Input
+            type="email"
+            required
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
         </Field>
         <Field label="Password">
-          <Input type="password" />
+          <Input
+            type="password"
+            required
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
         </Field>
         <Button type="submit">Sign In</Button>
       </form>
